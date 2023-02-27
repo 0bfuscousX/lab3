1f147c3660c397ceffe3dc567131a7318459f49d
0bfuscousX <lvlim@student.apc.edu.ph>
2023-02-28 00:40:18 +0800

    https://github.com/0bfuscousX/lab3.git

---
+++ ci4/app/Controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Damn, we don't have a page for that! Try again?
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+    }
+}
---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,11 @@
+This is my home page - Toby Lim
+
+<style>
+body {
+  background-image: url('https://c4.wallpaperflare.com/wallpaper/754/792/999/consoles-power-chainsaw-man-blonde-denji-chainsaw-man-chainsaw-man-hd-wallpaper-preview.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+
+}
+</style>
