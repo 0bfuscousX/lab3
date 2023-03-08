128dcc818f78570890d3e1ac86aef10da8919911
0bfuscousX <lvlim@student.apc.edu.ph>
2023-03-08 21:22:19 +0800

    backup

---
+++ bup/ci4/public/index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>OKUR0BOXUNE</title>
+  <link rel="icon" type="image/x-icon" href="assets/img/0bfuscousXLogo.png">	
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="HTML5 website template">
+  <meta name="keywords" content="global, template, html, sass, jquery">
+  <meta name="author" content="Bucky Maler">
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- notification for small viewports and landscape oriented smartphones -->
+<div class="device-notification">
+  <a class="device-notification--logo" href="https://www.youtube.com/shorts/YJWSAJjkTsI">
+    <img src="assets/img/0bfuscousXLogo.png" alt="ØB">
+    <p>OKUR0BOXUNE</p>
+  </a>
+  <p class="device-notification--message">Come one, too much! I must request you orient your device to portrait or find a larger screen. You won't be disappointed.</p>
+</div>
+
+<div class="perspective effect-rotate-left">
+  <div class="container"><div class="outer-nav--return"></div>
+    <div id="viewport" class="l-viewport">
+      <div class="l-wrapper">
+        <header class="header">
+          <a class="header--logo" href="https://www.youtube.com/shorts/YJWSAJjkTsI">
+            <img src="assets/img/0bfuscousXLogo.png" alt="ØB">
+            <p>OKUR0BOXUNE</p>
+          </a>
+          <button class="header--cta cta">Hire Me</button>
+          <div class="header--nav-toggle">
+            <span></span>
+          </div>
+        </header>
+        <nav class="l-side-nav">
+          <ul class="side-nav">
+            <li class="is-active"><span>Home</span></li>
+            <li><span>Socials</span></li>
+            <li><span>Merchandise</span></li>
+            <li><span>Contact</span></li>
+            <li><span>Hire Me</span></li>
+          </ul>
+        </nav>
+        <ul class="l-main-content main-content">
+          <li class="l-section section section--is-active">
+            <div class="intro">
+              <div class="intro--banner">
+                <h1>Welcome to <br> my world!<br> <br>I'm Toby Lim.</h1></a>
+                <button class="cta">Hire Me
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
+                  <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                    <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"/>
+                  </g>
+                  </svg>
+                  <span class="btn-background"></span>
+                </button>
+                <a href="https://open.spotify.com/user/22p25mpug7r6547yshhvukeiy"><img src="assets/img/introduction-visual.png" alt="Welcome"></a>
+              </div>
+              <div class="intro--options">
+                <a href="https://github.com/0bfuscousX">
+                  <h3>IT Specialist</h3>
+                  <p>2nd Year, [BSIT-MI]<br> Bachelor of Science in Information & Technology with specialization in mobile and internet, <br>Asia Pacific College</p>
+                </a>
+                <a href="https://www.w3profile.com/okuroboxune">
+                  <h3>Novice Graphic Designer</h3>
+                  <p>Basic knowledge in Photoshop, CSS, Canva</p>
+                </a>
+                <a href="https://open.spotify.com/user/22p25mpug7r6547yshhvukeiy">
+                  <h3>Saxophonist &amp; Keyboardist</h3>
+                  <p>Asia Pacific College Band multi-instrumentalist</p>
+                </a>
+              </div>
+            </div>
+          </li>
+          <li class="l-section section">
+            <div class="work">
+              <h2>Selected Socials</h2>
+              <div class="work--lockup">
+                <ul class="slider">
+                  <li class="slider--item slider--item-center">
+                    <a href="https://www.linkedin.com/in/lorenz-tobias-lim-41872021a/">
+                      <div class="slider--item-image">
+                        <img src="assets/img/linkedin-logo.png" alt="LinkedIn">
+                      </div>
+                      <p class="slider--item-title">LinkedIn</p>
+                      <p class="slider--item-description">Lorenz Tobias Lim, <br> 2nd Year BSIT-MI</p>
+                    </a>
+                  </li>
+                  <li class="slider--item slider--item-next">
+                    <a href="https://www.instagram.com/tobaltolim/">
+                      <div class="slider--item-image">
+                        <img src="assets/img/instagram-logo.png" alt="Instagram">
+                      </div>
+                      <p class="slider--item-title">Instagram</p>
+                      <p class="slider--item-description"><i>@tobaltolim<br><strong>[Peeve Highlight]</strong></i></p>
+                    </a>
+                  </li>
+                  <li class="slider--item slider--item-next">
+                    <a href="https://discord.gg/W5Fem9s82W">
+                      <div class="slider--item-image">
+                        <img src="assets/img/discord-logo.png" alt="Discord">
+                      </div>
+                      <p class="slider--item-title">Discord</p>
+                      <p class="slider--item-description">Æηιgмå, <br><i>@yukiko #8791</i><br>ワクワク</p>
+                    </a>
+                  </li>
+                  <li class="slider--item slider--item-next">
+                    <a href="https://steamcommunity.com/profiles/76561198282290849/">
+                      <div class="slider--item-image">
+                        <img src="assets/img/steam-logo.png" alt="Steam">
+                      </div>
+                      <p class="slider--item-title">Steam</p>
+                      <p class="slider--item-description">ObfuscousX <br><i>okuroboxune, inc.</i></p>
+                    </a>
+                  </li>
+                  <li class="slider--item slider--item-next">
+                    <a href="https://medal.tv/u/%25c3%2598bfuscousx">
+                      <div class="slider--item-image">
+                        <img src="assets/img/medal-logo.png" alt="Medal">
+                      </div>
+                      <p class="slider--item-title">Medal</p>
+                      <p class="slider--item-description">ØbfuscousX <br><strong>240 clips</strong></p>
+                    </a>
+                  </li>
+                </ul>
+                <div class="slider--prev">
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                  viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
+                  <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                    <path d="M561,1169C525,1155,10,640,3,612c-3-13,1-36,8-52c8-15,134-145,281-289C527,41,562,10,590,10c22,0,41,9,61,29
+                    c55,55,49,64-163,278L296,510h575c564,0,576,0,597,20c46,43,37,109-18,137c-19,10-159,13-590,13l-565,1l182,180
+                    c101,99,187,188,193,199c16,30,12,57-12,84C631,1174,595,1183,561,1169z"/>
+                  </g>
+                  </svg>
+                </div>
+                <div class="slider--next">
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
+                  <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                    <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"/>
+                  </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="l-section section">
+            <div class="about">
+              <div class="about--banner">
+                <h2>Need cat food?<br>Enjoyed my website?<br>Check out these<br>links for merch!</h2>
+                <a href="https://www.linkedin.com/in/lorenz-tobias-lim-41872021a/">Career
+                  <span>
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
+                    <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                      <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"/>
+                    </g>
+                    </svg>
+                  </span>
+                </a>
+                <img src="assets/img/about-visual.png" alt="About Us">
+              </div>
+              <div class="about--options">
+                <a href="https://www.facebook.com/feathersandfurs22">
+                  <h3>FEEDS</h3>
+                </a>
+                <a href="https://streamelements.com/0bfuscousx/tip">
+                  <h3>STREAM</h3>
+                </a>
+                <a href="https://instagram.com/gigatakoyaki_bantayancebu?igshid=YmMyMTA2M2Y=">
+                  <h3>TAKOYAKI</h3>
+                </a>
+              </div>
+            </div>
+          </li>
+          <li class="l-section section">
+            <div class="contact">
+              <div class="contact--lockup">
+                <div class="modal">
+                  <div class="modal--information">
+                    <p>Parañque City, NCR, Philippines</p>
+                    <a href="tel:+639667824062">+63 966 782 4062</a>
+                    <a href="mailto:lvlim@student.apc.edu.ph">lvlim@student.apc.edu.ph</a>
+                  </div>
+                  <ul class="modal--options">
+                    <li><a href="https://www.google.com/maps/place/Bermuda+Triangle/@27.3853087,-68.284198,4.71z/data=!4m6!3m5!1s0x89451ab5034cb7ab:0xb600ecf3df7aca4d!8m2!3d25!4d-71!16zL20vMDFqcGw">Find Me</a></li>
+                    <li><a href="tel:+639667824062">Call Me</a></li>
+                    <li><a href="mailto:lvlim@student.apc.edu.ph">Contact Me</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="l-section section">
+            <div class="hire">
+              <h2>You need me to do something? Click any!</h2>
+              <!-- checkout formspree.io for easy form setup -->
+              <form class="work-request">
+                <div class="work-request--options">
+                  <span class="options-a">
+                    <input id="opt-1" type="checkbox" value="app design">
+                    <label for="opt-1">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      App Development
+                    </label>
+                    <input id="opt-2" type="checkbox" value="graphic design">
+                    <label for="opt-2">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      Graphic Design
+                    </label>
+                    <input id="opt-3" type="checkbox" value="motion design">
+                    <label for="opt-3">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      Linux Servers
+                    </label>
+                  </span>
+                  <span class="options-b">
+                    <input id="opt-4" type="checkbox" value="ux design">
+                    <label for="opt-4">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      UX Design
+                    </label>
+                    <input id="opt-5" type="checkbox" value="webdesign">
+                    <label for="opt-5">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      Webdesign
+                    </label>
+                    <input id="opt-6" type="checkbox" value="marketing">
+                    <label for="opt-6">
+                      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      viewBox="0 0 150 111" style="enable-background:new 0 0 150 111;" xml:space="preserve">
+                      <g transform="translate(0.000000,111.000000) scale(0.100000,-0.100000)">
+                        <path d="M950,705L555,310L360,505C253,612,160,700,155,700c-6,0-44-34-85-75l-75-75l278-278L550-5l475,475c261,261,475,480,475,485c0,13-132,145-145,145C1349,1100,1167,922,950,705z"/>
+                      </g>
+                      </svg>
+                      Marketing
+                    </label>
+                  </span>
+                </div>
+                <div class="work-request--information">
+                  <div class="information-name">
+                    <input id="name" type="text" spellcheck="false">
+                    <label for="name">Name</label>
+                  </div>
+                  <div class="information-email">
+                    <input id="email" type="email" spellcheck="false">
+                    <label for="email">Email</label>
+                  </div>
+                </div>
+                <input type="submit" value="Send Request">
+              </form>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+  <ul class="outer-nav">
+    <li class="is-active">Home</li>
+    <li>Socials</li>
+    <li>Merchandise</li>
+    <li>Contact</li>
+    <li>Hire Me!</li>
+  </ul>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
+<script src="assets/js/functions-min.js"></script>
+</body>
+</html>
