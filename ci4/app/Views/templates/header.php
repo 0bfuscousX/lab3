c9492d1f87a0f1d11d45a1777f06de46fa1e69f7
0bfuscousX <lvlim@student.apc.edu.ph>
2023-03-09 21:19:33 +0800

    APC_WEBPROG_MI211

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <title>OKUR0BOXUNE's Guests</title>
+  <title>OKUR0BOXUNE</title>
   <link rel="icon" type="image/x-icon" href="assets/img/0bfuscousXLogo.png">	
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
