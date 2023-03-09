d57a3a4861fa701cc80b0c4414c25464e6d9783a
0bfuscousX <lvlim@student.apc.edu.ph>
2023-03-09 17:37:59 +0800

    guests

--- ci4/app/Views/welcome_message.php
+++ ci4/app/Views/welcome_message.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>OKUR0BOXUNE</title>
+    <link rel="icon" type="image/x-icon" href="public/favicon.ico">	
+
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
