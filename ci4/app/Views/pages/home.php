dff6c771067db5a02e75ecba2596484463bb62c6
0bfuscousX <lvlim@student.apc.edu.ph>
2023-03-09 17:59:25 +0800

    guests

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -275,35 +275,8 @@
                   </div>
                 </div>
 
-                <!--GUESTS--<?= session()->getFlashdata('error') ?>
-                <form action="create" method="post">
-                    <?= csrf_field() ?>
-
-                    <label for="name">Name</label>
-                    <input type="input" name="name" value="<?= set_value('name') ?>">
-                    <br>
-
-                    <label for="email">Email</label>
-                    <input type="input" name="email" value="<?= set_value('email') ?>">
-                    <br>
-
-                    <label for="website">Website</label>
-                    <input type="input" name="website" value="<?= set_value('website') ?>">
-                    <br>
-
-                    <label for="comment">Comment</label>
-                    <textarea name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
-                    <br>
-
-                    <label for="gender">Gender</label>
-                    <input type="input" name="gender" value="<?= set_value('gender') ?>">
-                    <br>
-
-                    <input type="submit" name="submit" value="Create guest entry">
-                </form> -->
-
                 <!--PHP-->
-  	  <?php
+  	        <?php
             // define variables and set to empty values
             $name = $email = $gender = $comment = $website = "";
 
@@ -364,7 +337,7 @@
               }
             }
             $conn->close();
-      ?>
+            ?>
                 
                 <input type="submit" value="Send Request">
               </form> 
